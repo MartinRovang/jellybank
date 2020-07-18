2bb5310288522ce1338e6ce56b2a726ceee39397
Martin Røvang <martinrovang@gmail.com>
2020-07-18 13:51:57 +0200

    start of project

---
+++ deploy.py
@@ -0,0 +1,16 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort, make_response, send_file, jsonify, Response
+import json
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+
+if __name__ == "__main__":
+    app.run()
