8f85ff3aa44ce0551910d6150075260e9306c7cd
Joakim <jwa018@post.uit.no>
2020-07-25 17:48:56 +0200

    Added database

--- deploy.py
+++ deploy.py
@@ -1,20 +1,22 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort, make_response, send_file, jsonify, Response
-import json
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
+
 @app.route('/about')
 def about():
     return render_template("about.html")
 
 
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-    
-
-
