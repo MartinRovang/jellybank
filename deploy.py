dc657abbf56594178d9e1dceeb301dfd5fb93ca7
Joakim <jwa018@post.uit.no>
2020-07-26 02:54:34 +0200

    Added "secret" key, and login/register routes

--- deploy.py
+++ deploy.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SECRET_KEY'] = 'gfYk2y14zL6DqVafCP4zu7SEqH8uoU0VI'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("home.html")
 
@@ -16,6 +20,16 @@ def about():
     return render_template("about.html")
 
 
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == "__main__":
